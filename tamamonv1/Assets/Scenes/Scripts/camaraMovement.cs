12a969cc38417c375baaa15ad9f646d5a2cab11c
anoderpersona <tamibadilla@gmail.com>
2021-10-13 16:37:42 -0300

    Camara lista y otra textura cielo

---
+++ tamamonv1/Assets/Scenes/Scripts/camaraMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraMovement : MonoBehaviour
+{
+
+    float mouseX;
+    public float mouseSensitivity = 250f;
+    public Transform cameraBody;
+    public float maxAngle = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Cursor.visible = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Necesita estar relativo a los fps y poder manejar la sensibilidad
+        
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        
+        if (cameraBody.rotation.y < maxAngle/100 && cameraBody.rotation.y > -maxAngle/100){
+            cameraBody.Rotate(Vector3.up * mouseX);
+        }
+        
+        else if (cameraBody.rotation.y >= maxAngle/100){
+            if (mouseX < 0){
+                cameraBody.Rotate(Vector3.up * mouseX);
+            }    
+        }
+        
+        else if (cameraBody.rotation.y <= -maxAngle/100){
+            if (mouseX > 0){
+                cameraBody.Rotate(Vector3.up * mouseX);
+            }    
+        }
+        
+        
+       // else cameraBody.rotation  = Quaternion.Euler(0.0f, maxAngle, 0.0f); 
+    }
+}
